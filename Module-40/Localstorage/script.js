1e9fe98d784090ad922bf39a116f764db264360e
fahmida-fahmi <abdulaziz21298@gmail.com>
2023-07-28 00:28:19 +0600

    Milestone-7

---
+++ Module-40/Localstorage/script.js
@@ -0,0 +1,13 @@
+// document.getElementById('btn').addEventListener('click',function(){
+
+//     const para = document.getElementById('para').innerText
+//     localStorage.setItem('id',parseInt(para))
+//     console.log();
+// })
+
+// click()
+
+const btn = () =>{
+    const para = document.getElementById('para').innerText
+    localStorage.setItem('name',parseInt(para))
+}
